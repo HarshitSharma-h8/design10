c98c106e9c9d06e9f64f9176264569495efa5773
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-12-02 16:38:27 +0530

    add blog page using pagination and collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,22 @@
-module.exports = function(eleventyConfig){
-    eleventyConfig.addPassthroughCopy('./src/style/style.css')
-    eleventyConfig.addPassthroughCopy('./src/javascript/index.js')
-    eleventyConfig.addPassthroughCopy('./src/assets')
-    
-    
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style/style.css");
+  eleventyConfig.addPassthroughCopy("./src/javascript/index.js");
+  eleventyConfig.addPassthroughCopy("./src/assets");
 
-    return {
-        dir :{
-            input : "src",
-            output : "_site"
-        }
-    }
-}
+  eleventyConfig.addCollection("blogPosts", function (collectionApi) {
+    const blogs = require("./src/_data/blogs.json");
+    return blogs.map((blog) => {
+      return {
+        data: blog,
+        slug: blog.slug,
+      };
+    });
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
