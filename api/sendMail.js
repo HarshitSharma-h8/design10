9f36accbab4c06c36f81f7f3b1e0389c94259108
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-12-19 07:53:01 +0530

    try to fix send mail

--- api/sendMail.js
+++ api/sendMail.js
@@ -35,6 +35,6 @@ export default async function handler(req, res) {
     return res.status(200).json({ message: 'Email sent successfully!' });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Failed to send email' });
+    return res.status(500).json({ error: `Failed to send email ${error}` });
   }
 }
